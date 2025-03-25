00edac0e58f4a68c7d6855cb11898d63052bffc4
GokilaSundar <gokithilaga267@gmail.com>
2025-03-25 19:49:29 +0530

    Implement url scrapper to json

---
+++ index.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { writeFile } from "fs/promises";
+import { parse } from "node-html-parser";
+
+import { urls } from "./urls.js";
+
+async function getPost(url) {
+  const response = await axios.get(url, {
+    responseType: "text",
+  });
+
+  const html = response.data;
+
+  const document = parse(html);
+
+  const titleElement = document.querySelector("span.mw-page-title-main");
+  const contentElement = document.getElementById("bodyContent");
+
+  if (!titleElement || !contentElement) {
+    throw new Error("Failed to parse post");
+  }
+
+  const title = titleElement.text;
+  const content = contentElement.text;
+
+  return { title, content, url };
+}
+
+async function main() {
+  const posts = [];
+
+  for (let i = 0; i < urls.length; i++) {
+    console.log(`Fetching post ${i + 1} of ${urls.length}`);
+    try {
+      const post = await getPost(urls[i]);
+
+      posts.push(post);
+
+      console.log("Success");
+    } catch (error) {
+      console.error("Failed to fetch post", error);
+    }
+  }
+
+  console.log("All posts fetched");
+
+  await writeFile("posts.json", JSON.stringify(posts, null, 2));
+
+  console.log("Posts written to posts.json");
+
+  process.exit(0);
+}
+
+main().catch((error) => {
+  console.error("Failed to fetch posts", error);
+  process.exit(1);
+});
